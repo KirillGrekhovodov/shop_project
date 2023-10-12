3fc58481704aed861a443ea9f73aa72450dc264b
”Kirill <kirillgrekhovodov@gmail.com>
2023-10-12 20:39:38 +0600

    заменил подключение к базе sqlite3

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -79,7 +79,10 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db(),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
